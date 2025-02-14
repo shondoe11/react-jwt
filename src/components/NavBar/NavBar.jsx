25f76b391819ff6c9485bbfb4a91ed9f403035ae
shondoe11 <shawnhadritan@gmail.com>
2025-02-14 09:29:58 +0800

    complete

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,15 @@
 // src/components/NavBar/NavBar.jsx
 
+import { Link } from 'react-router';
+
 const NavBar = () => {
   return (
     <nav>
-      <p>Navbar contents will go here.</p>
+      <ul>
+        <li><Link to='/sign-up'>Sign Up</Link></li>
+      </ul>
     </nav>
   );
 };
 
 export default NavBar;
-
