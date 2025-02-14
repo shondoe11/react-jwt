2758a66465abc6893880fc434321fc388b875af1
shondoe11 <shawnhadritan@gmail.com>
2025-02-14 10:11:30 +0800

    complete

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,100 @@
+// SignUpForm.jsx
+
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+const SignUpForm = () => {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    passwordConf: '',
+  });
+
+  const { username, password, passwordConf } = formData;
+
+  const handleChange = (evt) => {
+    setMessage('');
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    // console.log(formData); // this line will print the form data to the console
+    //? fire the fetch
+    const url = 'http://localhost:3000/auth/sign-up';
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        });
+        const json = await response.json();
+        console.log(json);
+        if (!response.ok) {
+        throw new Error(json.err);
+        }
+        //? save into localstorage
+        localStorage.setItem('token', json.token);
+    } catch (error) {
+        console.error(error.message);
+        setMessage(error.message);
+  }
+  };
+
+
+  const isFormInvalid = () => {
+    return !(username && password && password === passwordConf);
+  };
+
+  return (
+    <main>
+      <h1>Sign Up</h1>
+      <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            id='name'
+            value={username}
+            name='username'
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            value={password}
+            name='password'
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='confirm'>Confirm Password:</label>
+          <input
+            type='password'
+            id='confirm'
+            value={passwordConf}
+            name='passwordConf'
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button disabled={isFormInvalid()}>Sign Up</button>
+          <button onClick={() => navigate('/')}>Cancel</button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default SignUpForm;
